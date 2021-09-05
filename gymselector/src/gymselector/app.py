398ee1578f26647bcc833fec4701c68aa44f3a31
moondial <lp@luispal.com>
2021-09-05 12:26:23 -0700

    print button results in app achieved. Resize main_box to be smaller

--- gymselector/src/gymselector/app.py
+++ gymselector/src/gymselector/app.py
@@ -4,38 +4,48 @@ Random climbing gym selector
 import random
 import toga
 from toga.style import Pack
-from toga.style.pack import COLUMN, ROW
+from toga.style.pack import COLUMN, ROW, LEFT, RIGHT
 
 
-class GymSelector(toga.App):
-    def startup(self):
-        main_box = toga.Box(style=Pack(direction=COLUMN))
+def build(app):
+    main_box = toga.Box()
+    gym_box = toga.Box()
 
+    gym_input = toga.TextInput(readonly=True)
 
-        button = toga.Button(
-                    'Pick a gym at random',
-                    on_press=self.random_gym,
-                    style=Pack(padding=5)
-        )
+    gym_label = toga.Label('Pull Down Here: ', style=Pack(text_align=LEFT))
 
-        #gym_label = toga.Label("Pull down here: ", text="")
-
-        main_box.add(button)
-
-        self.main_window = toga.MainWindow(title=self.formal_name)
-        self.main_window.content = main_box
-        self.main_window.show()
-
-    def random_gym(self, widget):
+    def random_gym(widget):
         d = [
             "Vertical Hold: San Marcos", "Vertical Hold: Old Town", "Vertical Hold: Poway", "Mesa Rim: North",
             "Mesa Rim: San Diego", "Mesa Rim: Mira Mesa", "The Wall", "Vital: Carlsbad", "Vital: Oceanside"
         ]
         select = random.choice(d)
-        print(select)
+        gym_input.value = select
+
+
+    button = toga.Button('Choose Gym at Random', on_press=random_gym)
+
+    gym_box.add(gym_label)
+    gym_box.add(gym_input)
+
+    main_box.add(gym_box)
+    main_box.add(button)
+
+    main_box.style.update(direction=COLUMN, padding_top=10)
+    gym_box.style.update(direction=ROW, padding=5)
+
+    gym_input.style.update(flex=1)
+    gym_label.style.update(width=100, padding_left=10)
+
+    button.style.update(padding=15, flex=1)
+
+    return main_box
 
 
 def main():
-    return GymSelector()
+    return toga.App('Gym Selector', 'org.gym_selector.gs', startup=build)
 
 
+if __name__ == '__main__':
+    main().main_loop()
